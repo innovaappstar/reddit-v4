cb11c5131c2840a4eb9ec85c75e09c9df4f95801
unknown <innovaappstar@gmail.com>
2019-07-11 22:47:42 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Article } from './article/article.model';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-reddit';
+  listArticles: Article[]; // <-- component property
+
+  constructor() {
+    this.listArticles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
+  sortedArticles(): Article[] {
+    return this.listArticles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
+
+  /**
+   * Nota : Uso de 'template variable' en el html..
+   *
+   * método para agregar interacción entre vista y controlador..
+   * @param title input text
+   * @param link input text
+   */
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.listArticles.push(new Article(title.value, link.value));
+    title.value = '';
+    link.value = '';
+    this.listArticles.sort((a : Article, b : Article)=>a.votes - b.votes)
+    return false;
+  }
+
 }
